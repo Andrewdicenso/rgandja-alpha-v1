3cd62a2e401f88935f20343f6ff26ad22bbbe475
Andrewdicenso <andrewdicenso@libero.it>
2026-08-18 22:49:46 +0300

    fare

--- genera_hash.py
+++
@@ -1,7 +0,0 @@
-import bcrypt
-
-password_da_usare = "LA_TUA_PASSWORD_SCELTA"
-salt = bcrypt.gensalt()
-hashed = bcrypt.hashpw(password_da_usare.encode('utf-8'), salt)
-
-print(f"Ecco l'hash da copiare nel database: {hashed.decode('utf-8')}")
